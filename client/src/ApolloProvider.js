69960cda6fb69fd38aef8787cc72fcffef148c91
Jose Mejia <mejialaguna@yahoo.com>
2022-01-26 11:10:52 -0500

    net

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from "@apollo/react-hooks";
 import { setContext } from "apollo-link-context"; // using apollo-link-context
 
 const httpLink = createHttpLink({
-  uri: "https://polar-citadel-75841.herokuapp.com/",
+  uri: "/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
